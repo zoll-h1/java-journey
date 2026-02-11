630f195a9ff9538f74d7b232a36a1b8a213fc09c
zoll-h1 <nurbekerkibaev9@gmail.com>
2026-02-11 21:48:44 +0600

    Not really neccessary changes

---
+++ Bro-Code-Java/quizGame.java
@@ -0,0 +1,15 @@
+public class quizGame {
+    public static void main(String[] args) {
+        // JAVA QUIZ GAME
+        String[] questions = {"What is the main function of a router ?",
+                              "Which part of the computer is considered to brain?",
+                              "What year was Facebook launched?",
+                              "Who is known as a father of the computer?",
+                              "What was the first programming language?"};
+        String[][] options = {{"1. ", "2. ", "3. ", "4. "},
+                              {"1. ", "2. ", "3. ", "4. "},
+                              {""},
+                              {""},
+                              {""}};
+    }
+}
